0a72ab1a6128604a83ff3de8e3549beb68d37135
GearsOfSmores <77704375+GearsOfSmores@users.noreply.github.com>
2021-10-04 18:19:46 -0400

    Added Iframes code

--- Quck_Wick/Assets/Scripts/Objects/GameManager.cs
+++ Quck_Wick/Assets/Scripts/Objects/GameManager.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Iframes")]
+    [SerializeField] private float IframesDuration;
+    [SerializeField] private int numberOfFlashes;
+    public SpriteRenderer spriteRend;
+    private void Start()
     {
-        
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+    void Hit()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator Invulnerability()
     {
-        
+        Physics2D.IgnoreLayerCollision(9, 10, true);
+        //invulnerability duration
+
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(IframesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(IframesDuration / (numberOfFlashes * 2));
+        }
+
+        Physics2D.IgnoreLayerCollision(9, 10, false);
     }
+
+
 }
