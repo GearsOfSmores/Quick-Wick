5a22e8b090d2149fa79aaadd3ed9ba72559af153
shane504 <78117040+shane504@users.noreply.github.com>
2021-10-08 11:45:13 -0700

    Add files via upload
    Added the checkpoints code along with gamemaster to save the player location when touching a checkpoint. Also added enemy movement that detects when they are on a platform and if they run into walls

---
+++ Quck_Wick/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private bool keepPatrolling;
+
+    // To set the enemy move speed in the inspector
+    public float moveSpeed;
+
+    private bool turn;
+    private bool wallTurn;
+
+    // To set the enemy rigid body in the inspector
+    public Rigidbody2D rb;
+
+    // The public variables to set the ground detection child and the wall detection child
+    public Transform detectGoundPos;
+    public Transform detectWallPos;
+
+    // To set what the detections are looking for and the box collider
+    // tried removing the collider but had issues with them going through walls when removed
+    public LayerMask groundLayer;
+    public Collider2D bodyCollider;
+
+    void Start()
+    {
+        keepPatrolling = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If keep patrolling equals true then the enemy will keep going
+        if (keepPatrolling)
+        {
+            Patrol();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (keepPatrolling)
+        {
+            // If the enemy touches a wall or reaches the end of a platform then change there direction
+            turn = !Physics2D.OverlapCircle(detectGoundPos.position, 0.1f, groundLayer);
+            wallTurn = Physics2D.OverlapCircle(detectWallPos.position, 0.1f, groundLayer);
+        }
+    }
+
+    private void Patrol()
+    {
+        // If the enemy hits anything or if they reach the end of the platform turn the enemy
+        if (turn || bodyCollider.IsTouchingLayers(groundLayer) || wallTurn)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(moveSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    private void Flip()
+    {
+        // To flip the enemy left and right using the local scale x
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        moveSpeed *= -1;
+        keepPatrolling = true;
+    }
+
+
+}
