b7de19ad526965812baf1c2f7a508fa0ce6f378d
shane504 <78117040+shane504@users.noreply.github.com>
2021-10-15 12:04:24 -0700

    Add files via upload
    Made changes to the flipsprite, respond, update, pull, and stop pulling functions to fix the issue with the respawning in the air if holding a crate during respawn. Also added a crate reset script so when the player respawns then so do the crates getting rid of the issue where if you fall off with the crate the crate disappears.

---
+++ Quck_Wick/Assets/Scripts/Player/CrateReset.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrateReset : MonoBehaviour
+{
+    public Vector2[] cratePos;
+    public GameObject crate;
+
+    // To check what Items are in the crate array
+    public GameObject[] crates;
+    int crateArrayLength;
+    
+    void Start()
+    {
+        int count = 0;
+
+        // This creates an array of all items tagged grabbable
+        crates = GameObject.FindGameObjectsWithTag("Grabbable");
+        crateArrayLength = crates.Length;
+
+        // This creates an array with the size of the previous array for crate position
+        cratePos = new Vector2[crateArrayLength];
+
+        // For each statement to get the position of each item tagged grabbable from the transform
+        foreach (GameObject pos in crates)
+        {
+            cratePos[count] = pos.transform.position;
+            count++;
+        }
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    // Function for if called will reset the position of all the crates in the scene
+    public void crateReset()
+    {
+        for(int i = 0; i < crates.Length; i++)
+        {
+            crates[i].transform.position = cratePos[i];
+        }
+    }
+}
