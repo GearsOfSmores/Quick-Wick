5f0ca59c880b47366061685103cde3cfd7722755
LiamKaufman <91220185+LiamKaufman@users.noreply.github.com>
2021-10-18 15:45:15 -0400

    Added Credits(Fixed)

--- Quck_Wick/Assets/Scripts/Main Menu/MainMenu.cs
+++ Quck_Wick/Assets/Scripts/Main Menu/MainMenu.cs
@@ -5,13 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject creditsSet;
+    public GameObject quitButton;
+    public GameObject creditsButton;
+    public GameObject backButton;
+
     public void StartButton()
     {
         SceneManager.LoadScene("TutorialBegin");
+
     }
 
-    public void QuitGame()
+    public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Credits()
+    {
+        creditsButton.SetActive(false);
+        quitButton.SetActive(false);
+        backButton.SetActive(true);
+        creditsSet.SetActive(true);
+    }
+
+    public void Back()
+    {
+        backButton.SetActive(false);
+        creditsSet.SetActive(false);
+        creditsButton.SetActive(true);
+        quitButton.SetActive(true);
     }
 }
